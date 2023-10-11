1a05b6efaffdde3dd8aa2ed67c6a4569fc30f02e
vinivasq <vinivasq@hotmail.com>
2023-10-11 08:10:36 -0300

    fixed connection string to production

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   apiURL: 'https://seamstress-backend-production.up.railway.app/api',
+  // apiURL: 'http://localhost:5279/api',
   imageAPI: 'https://seamstressstorage.blob.core.windows.net/images/models',
 };
