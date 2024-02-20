77e08d85d1193792edad45e09e2c4fd06f53fced
vinivasq <vinivasq@hotmail.com>
2024-02-19 21:21:56 -0300

    added orders table

--- src/app/routes/report/ordersReport/ordersReport.component.ts
+++ src/app/routes/report/ordersReport/ordersReport.component.ts
@@ -1,26 +1,70 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import {
   FormBuilder,
   FormControl,
   FormGroup,
   Validators,
 } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 import { ToastrService } from 'ngx-toastr';
 import { Observable, debounceTime, map, startWith, switchMap } from 'rxjs';
+import { DialogCarouselComponent } from 'src/app/components/dialogCarousel/dialogCarousel.component';
 import { Customer } from 'src/app/models/Customer';
+import { Order } from 'src/app/models/Order';
 import { OrderParams } from 'src/app/models/OrderParams';
 import { PageParams } from 'src/app/models/PageParams';
-import { PaginatedResult } from 'src/app/models/Pagination';
+import { PaginatedResult, Pagination } from 'src/app/models/Pagination';
 import { CustomerService } from 'src/app/services/customer.service';
 import { OrderService } from 'src/app/services/order.service';
 import { SpinnerService } from 'src/app/services/spinner.service';
+import { environment } from 'src/environments/environment';
+import { formatHeader } from 'src/helpers/ColumnHeaders';
 
 @Component({
   selector: 'app-ordersReport',
   templateUrl: './ordersReport.component.html',
   styleUrls: ['./ordersReport.component.scss'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({ height: '0px', minHeight: '0' })),
+      state('expanded', style({ height: '*' })),
+      transition(
+        'expanded <=> collapsed',
+        animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')
+      ),
+    ]),
+  ],
 })
 export class OrdersReportComponent implements OnInit {
+  public dataSource: any;
+  public foundOrders = false;
+  public searched = false;
+  public pagination = {
+    currentPage: 0,
+    pageSize: 10,
+    totalItems: 1,
+  } as Pagination;
+  displayedColumns: string[] = [
+    'id',
+    'customer',
+    'orderedAt',
+    'deadline',
+    'total',
+    'step',
+  ];
+  displayedColumnsWithExpand = [...this.displayedColumns, 'expand'];
+  expandedOrder?: Order | null;
+  imageAPI = environment.imageAPI;
   steps = [
     { label: 'Aguardando', value: 0 },
     { label: 'Corte', value: 1 },
@@ -50,11 +94,15 @@ export class OrdersReportComponent implements OnInit {
     steps: [[5]],
   });
 
+  @ViewChild(MatPaginator) paginator?: MatPaginator;
+  @ViewChild(MatSort) sort?: MatSort;
+
   constructor(
     private _formBuilder: FormBuilder,
     private _orderService: OrderService,
     private _customerService: CustomerService,
     private _toastrService: ToastrService,
+    private _dialog: MatDialog,
     private _spinnerService: SpinnerService
   ) {
     this.dateMinusSeven.setDate(new Date().getDate() - 7);
@@ -73,9 +121,13 @@ export class OrdersReportComponent implements OnInit {
     }
 
     this._spinnerService.isLoading = true;
+    this.searched = true;
 
     const { customer, ...formValues } = this.form.getRawValue();
-    const orderParams = new OrderParams(0, 25);
+    const orderParams = new OrderParams(
+      this.pagination.currentPage,
+      this.pagination.pageSize
+    );
     orderParams.customerId = formValues.customerId;
     orderParams.orderedAtStart = formValues.range.start;
     orderParams.orderedAtEnd = formValues.range.end;
@@ -85,6 +137,11 @@ export class OrdersReportComponent implements OnInit {
       .getOrders(orderParams)
       .subscribe({
         next: (data) => {
+          this.dataSource = new MatTableDataSource(data.result);
+          this.pagination = data.pagination;
+          this.dataSource.sort = this.sort;
+          if (data.result.length > 0) this.foundOrders = true;
+
           console.log(data);
         },
         error: (err: any) => {
@@ -124,5 +181,30 @@ export class OrdersReportComponent implements OnInit {
 
   getCustomerId(event, customerId: number) {
     if (event.isUserInput) this.form.get('customerId')?.setValue(customerId);
+  }
+
+  pageChange(event: PageEvent) {
+    this.pagination.currentPage = event.pageIndex;
+    this.pagination.pageSize = event.pageSize;
+
+    this.searchOrders();
+  }
+
+  checkCustomerValue() {
+    if (this.form.get('customer').value === '')
+      this.form.get('customerId').patchValue(null);
+  }
+
+  public openCarousel(imageURL: string) {
+    this._dialog.open(DialogCarouselComponent, {
+      data: {
+        title: `Fotos do Modelo`,
+        imageURL: imageURL,
+      },
+    });
+  }
+
+  public columnHeader(header: string): string {
+    return formatHeader(header);
   }
 }
