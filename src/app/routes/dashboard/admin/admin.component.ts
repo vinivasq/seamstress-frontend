4498db28f7bed2dd13655cd08cedfa82dad8cd07
vinivasq <vinivasq@hotmail.com>
2024-03-03 20:53:55 -0300

    charts and statistics mockup layout

--- src/app/routes/dashboard/admin/admin.component.ts
+++ src/app/routes/dashboard/admin/admin.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RegionChartComponent } from 'src/app/components/charts/region-chart/region-chart.component';
 import { MatCardModule } from '@angular/material/card';
+import { RevenueChartComponent } from 'src/app/components/charts/revenue-chart/revenue-chart.component';
 
 @Component({
   selector: 'app-admin',
   standalone: true,
-  imports: [CommonModule, RegionChartComponent, MatCardModule],
+  imports: [
+    CommonModule,
+    RegionChartComponent,
+    RevenueChartComponent,
+    MatCardModule,
+  ],
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.scss'],
 })
