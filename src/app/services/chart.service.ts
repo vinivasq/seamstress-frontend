ebdf5a3a27beda04a79934e53ebc912e322a77c8
vinivasq <vinivasq@hotmail.com>
2024-04-05 18:49:07 -0300

    getBarLine service with query params

--- src/app/services/chart.service.ts
+++ src/app/services/chart.service.ts
@@ -3,13 +3,14 @@ import { Injectable } from '@angular/core';
 import { Observable, take } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { DoughnutChart } from '../models/viewModels/charts/DoughnutChart';
+import { BarLineChart } from '../models/viewModels/charts/BarLineChart';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ChartService {
   constructor(private client: HttpClient) {}
-  baseURL = `${environment.apiURL}/chart/doughnut`;
+  baseURL = `${environment.apiURL}/chart`;
 
   getDoughnutChart(
     data: string,
@@ -23,7 +24,26 @@ export class ChartService {
     params = params.append('periodEnd', periodEnd);
 
     return this.client
-      .get<DoughnutChart>(this.baseURL, {
+      .get<DoughnutChart>(`${this.baseURL}/doughnut`, {
+        observe: 'response',
+        params,
+      })
+      .pipe(take(1));
+  }
+
+  getBarLineChart(
+    data: string,
+    periodBegin: string,
+    periodEnd: string
+  ): Observable<BarLineChart | HttpResponse<BarLineChart>> {
+    let params = new HttpParams();
+
+    params = params.append('data', data);
+    params = params.append('periodBegin', periodBegin);
+    params = params.append('periodEnd', periodEnd);
+
+    return this.client
+      .get<BarLineChart>(`${this.baseURL}/BarLine`, {
         observe: 'response',
         params,
       })
