63ee00da3d63928c3fafc3e7763fb6f9cc4979d2
vinivasq <vinivasq@hotmail.com>
2024-04-15 18:16:52 -0300

    getItemOrder and updateItemOrder service with ItemOrderInputDto

---
+++ src/app/dtos/ItemOrderInputDto.ts
@@ -0,0 +1,10 @@
+export class ItemOrderInputDto {
+  id: number;
+  itemId: number;
+  orderId: number;
+  colorId: number;
+  fabricId: number;
+  sizeId: number;
+  amount: number;
+  description: string;
+}
